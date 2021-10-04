85df767328f18d25c6e60848348db719c3217547
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-10-04 22:04:02 +0600

    task reminder style added

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,8 @@ import {FaTimes} from 'react-icons/fa';
 
 const Task = ({task, onDelete, reminder}) =>{
 return(
-  <div className="task" onDoubleClick={()=>reminder(task.id)}>
+  <div className= {`task ${task.reminder && 'reminder'}`}
+       onDoubleClick={()=>reminder(task.id)}>
     <h3>{task.text}
       <FaTimes style={{color: 'red'}}
                onClick={()=>onDelete(task.id)}
