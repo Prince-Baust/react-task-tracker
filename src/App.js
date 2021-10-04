3924668113b577de66eece8761626a8195050109
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-10-05 01:51:26 +0600

    db.json populated

--- src/App.js
+++ src/App.js
@@ -5,26 +5,7 @@ import AddTask from "./components/AddTask";
 
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctor\'s Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: true
-    },
-    {
-      id: 2,
-      text: 'Meeting at school',
-      day: 'Feb 6th at 2:30pm',
-      reminder: true
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder: false
-    }
-  ]);
+  const [tasks, setTasks] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
 
   // Add Task
