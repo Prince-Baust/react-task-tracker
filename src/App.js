a8cc1c94336e8db3de0969076903e41c4e6efce1
Md. Maruf Al Hossain Prince <pmaruf42@gmail.com>
2021-10-04 20:51:22 +0600

    Tasks component complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import Header from './components/Header'
+import Tasks from "./components/Tasks";
+import {useState} from "react";
 
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctor\'s Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Meeting at school',
+      day: 'Feb 6th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false
+    }
+  ]);
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks = {tasks}/>
     </div>
   );
 }
